2640a156d8a5c33fe7b65dcd1799841a773a0b16
Степан дыбо <dybosm@mail.ru>
2025-06-05 10:04:13 +0300

    xc

---
+++ HoomeWork/Task7_1/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Task7_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите первое число: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введите второе число: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Выберите операцию:\n");
+                Console.Write("1 — сложение\n");
+                Console.Write("2 — вычитание\n");
+                Console.Write("3 — умножение\n");
+                Console.Write("4 — деление\n");
+                Console.Write("Ваш выбор: ");
+                string operationInput = Console.ReadLine();
+                int operationCode = Convert.ToInt32(operationInput);
+
+                switch (operationCode)
+                {
+                    case 1:
+                        Console.WriteLine($"Результат сложения: {num1 + num2}");
+                        break;
+
+                    case 2:
+                        Console.WriteLine($"Результат вычитания: {num1 - num2}");
+                        break;
+
+                    case 3:
+                        Console.WriteLine($"Результат произведения: {num1 * num2}");
+                        break;
+
+                    case 4:
+                        if (num2 != 0)
+                            Console.WriteLine($"Частное чисел: {num1 / num2}");
+                        else
+                            throw new DivideByZeroException(); // Исключение при делении на ноль
+                        break;
+
+                    default:
+                        Console.WriteLine("Неверная операция.");
+                        return;
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Ошибка! Вы ввели неверные данные. Попробуйте снова.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Ошибка! Деление на ноль невозможно.");
+            }
+        }
+    }
+}
---
+++ HoomeWork/Task7_2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task7_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите Ваш возраст: ");
+                int userAge = int.Parse(Console.ReadLine());
+
+                ValidateAge(userAge);
+
+                Console.WriteLine("Ваш возраст корректен!");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message); 
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: введено неправильное значение возраста.");
+            }
+        }
+
+        public static void ValidateAge(int age)
+        {
+            if (age < 0)
+            {
+                throw new ArgumentException("Возраст не может быть отрицательным");
+            }
+            else if (age > 150)
+            {
+                throw new ArgumentOutOfRangeException("Слишком большой возраст"); 
+            }
+        }
+    }
+    
+}
